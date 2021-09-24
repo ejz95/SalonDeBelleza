2c4216a4838e5250bdbd51692650c20de5ebf7cd
BRYAN FRANCISCO ZAVALA MURILLO <bf.murillo@unah.hn>
2021-09-24 11:49:03 -0600

    Agregando arquitectura basica

--- SalonDeBelleza/SalonDeBelleza.web/Controllers/ProductosController.cs
+++ SalonDeBelleza/SalonDeBelleza.web/Controllers/ProductosController.cs
@@ -1,4 +1,4 @@
-﻿using SalonDeBelleza.web.Models;
+﻿using SalonDeBelleza.BL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,23 +12,9 @@ namespace SalonDeBelleza.web.Controllers
         // GET: Productos
         public ActionResult Index()
         {
-            var producto1 = new ProductoModel();
-            producto1.Id = 1;
-            producto1.Descripcion = "Corte de Cabello";
-
-            var producto2 = new ProductoModel();
-            producto2.Id = 2;
-            producto2.Descripcion = "Secado de Cabello";
-
-            var producto3 = new ProductoModel();
-            producto3.Id = 3;
-            producto3.Descripcion = "Planchado de Cabello";
-
-            var listadeProductos = new List<ProductoModel>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
+                        
             return View(listadeProductos);
         }
     }
---
+++ SalonDeBelleza/SalondeBelleza.WebAdmin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SalondeBelleza.WebAdmin.MvcApplication" Language="C#" %>
