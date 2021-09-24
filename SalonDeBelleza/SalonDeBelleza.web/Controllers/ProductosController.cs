8986cdec001c331c92a0488126ebf17b42c46fcd
BRYAN FRANCISCO ZAVALA MURILLO <bf.murillo@unah.hn>
2021-09-24 09:03:11 -0600

    agregamos lista de productos

---
+++ SalonDeBelleza/SalonDeBelleza.web/Controllers/ProductosController.cs
@@ -0,0 +1,35 @@
+﻿using SalonDeBelleza.web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SalonDeBelleza.web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var producto1 = new ProductoModel();
+            producto1.Id = 1;
+            producto1.Descripcion = "Corte de Cabello";
+
+            var producto2 = new ProductoModel();
+            producto2.Id = 2;
+            producto2.Descripcion = "Secado de Cabello";
+
+            var producto3 = new ProductoModel();
+            producto3.Id = 3;
+            producto3.Descripcion = "Planchado de Cabello";
+
+            var listadeProductos = new List<ProductoModel>();
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
+
+            return View(listadeProductos);
+        }
+    }
+}
