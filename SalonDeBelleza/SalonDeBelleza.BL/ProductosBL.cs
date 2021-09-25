acbfa5794b253b14ce4cc5326253a83083c5a3b3
BRYAN FRANCISCO ZAVALA MURILLO <bf.murillo@unah.hn>
2021-09-25 10:01:05 -0600

    agregando arquitectura basica segunda vez

---
+++ SalonDeBelleza/SalonDeBelleza.BL/ProductosBL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalonDeBelleza.BL
+{
+    public class ProductosBL
+    {
+        public List<Producto> OdtenerProductos()
+        {
+            var producto1 = new Producto();
+            producto1.Id = 1;
+            producto1.Descripcion = "Lavado y Secado";
+            producto1.Precio = 150;
+
+            var producto2 = new Producto();
+            producto2.Id = 2;
+            producto2.Descripcion = "Lavado Tratamiento y Planchado";
+            producto2.Precio = 250;
+
+            var producto3 = new Producto();
+            producto3.Id = 3;
+            producto3.Descripcion = "Corte";
+            producto3.Precio = 150;
+
+            var listadeProductos = new List<Producto>();
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
+
+            return listadeProductos;
+        }
+    }
+}
--- SalonDeBelleza/SalonDeBelleza.web/Controllers/ProductosController.cs
+++ SalonDeBelleza/SalonDeBelleza.web/Controllers/ProductosController.cs
@@ -1,4 +1,4 @@
-﻿using SalonDeBelleza.Web.Models;
+﻿using SalonDeBelleza.BL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,22 +12,8 @@ namespace SalonDeBelleza.Web.Controllers
         // GET: Productos
         public ActionResult Index()
         {
-            var producto1 = new ProductoModel();
-            producto1.Id = 1;
-            producto1.Descripcion = "Lavado y Secado";
-
-            var producto2 = new ProductoModel();
-            producto2.Id = 2;
-            producto2.Descripcion = "Lavado Tratamiento y Planchado";
-
-            var producto3 = new ProductoModel();
-            producto3.Id = 3;
-            producto3.Descripcion = "Corte";
-
-            var listadeProductos = new List<ProductoModel>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.OdtenerProductos();
 
             return View(listadeProductos);
         }
